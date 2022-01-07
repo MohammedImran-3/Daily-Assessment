0b0dc46c3441cb1d76a26a304961516b4a394f26
MohammedImran-3 <mohammed.imran@indiumsoft.com>
2022-01-07 16:45:17 +0530

    Assignment-6 Completed

---
+++ Assignment-6/src/org/test/Salaries.java
@@ -0,0 +1,40 @@
+package org.test;
+
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Salaries {
+	public static void main(String[] args) {
+		double highest = 0;
+		double lowest = 0;
+		int count = 0;
+		HashSet set = new HashSet();
+		Scanner s = new Scanner(System.in);
+		while (true) {
+			char i = s.next().charAt(0);
+			if (i == 'y') {
+				System.out.println("Enter salary");
+				double sal = s.nextInt();
+				set.add(sal);
+			} else if (i == 'n')
+				break;
+			else
+				System.out.println("Wrong input enter y or n");
+		}
+		System.out.println(set);
+		for (Object o : set) {
+			if ((double) o > 30000)
+				count++;
+			if ((double) o > highest)
+				highest = (double) o;
+			if (lowest == 0)
+				lowest = (double) o;
+			if ((double) o < lowest)
+				lowest = (double) o;
+
+		}
+		System.out.println("The no: of employees getting salary >30000 are: " + count);
+		System.out.println("The highest salary is " + highest + " and the lowest salary is " + lowest);
+	}
+
+}
